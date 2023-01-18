50b246539e6b897df78121afe1d42931d66d260d
Michael Griebling <sam@c-inspirations.com>
2023-01-18 16:14:41 -0500

    Initial Commit

---
+++ SwiftMathDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftMathDemo
+//
+//  Created by Mike Griebling on 2023-01-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftMathDemo/SwiftMathDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMathDemoApp.swift
+//  SwiftMathDemo
+//
+//  Created by Mike Griebling on 2023-01-18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMathDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
